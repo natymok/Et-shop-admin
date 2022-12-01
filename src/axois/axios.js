cccb1ca3e96782fccdd8b198504e3687238953b8
Natnael Mekonnen <naty@nany.zte.com.cn>
2022-12-01 06:30:09 -0500

    first commit

---
+++ src/axois/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+const _token=localStorage.getItem('user')
+const axiosinstance=axios.create({
+    baseURL:"https://etshop-server.onrender.com/api",
+    headers:{
+        "Authorization":_token?_token:''
+    }
+})
+
+export default axiosinstance;
