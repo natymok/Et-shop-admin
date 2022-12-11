8a613476cc08dc02614a979927305f5a996890b2
Natnael Mekonnen <naty@nany.zte.com.cn>
2022-12-11 08:22:12 -0500

    change home url to sign in

--- src/components/header.js
+++ src/components/header.js
@@ -14,9 +14,9 @@
       <div className="flex justify-between items-center p-2 pl-5">
         <Link to ='/' className="text-white">Admin   Dashboard</Link>
         <div className="flex justify-between items-center w-[10%] mr-5">
-       { !token && <Link to="/signin" className="text-white">Signin</Link>}
+       { !token && <Link to="/signin" className="text-white">Sign in</Link>}
       { token &&  <button onClick={logout} className="text-white font-serif text-lg">signOut</button>}
-       { !token && <Link to='/signup' className="text-white"> signup</Link>}
+       { !token && <Link to='/signup' className="text-white"> sign up</Link>}
 
         </div>
       </div>
