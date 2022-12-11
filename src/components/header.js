6ae03da58be4fa60c7e976d2f436f9d6a4150390
Natnael Mekonnen <naty@nany.zte.com.cn>
2022-12-11 08:17:36 -0500

    update haeder

--- src/components/header.js
+++ src/components/header.js
@@ -8,15 +8,15 @@
       localStorage.clear()
       history.push('/signin')
     }
-    const [user,dispatch]=useStateValue()
+    const [{token},dispatch]=useStateValue()
     return ( 
     <div className="w-full h-[50px] bg-slate-700 ">
       <div className="flex justify-between items-center p-2 pl-5">
         <Link to ='/' className="text-white">Admin   Dashboard</Link>
         <div className="flex justify-between items-center w-[10%] mr-5">
-       { !user && <Link to="/signin" className="text-white">Signin</Link>}
-      { user &&  <button onClick={logout} className="text-white font-serif text-lg">signOut</button>}
-       { !user && <Link to='/signup' className="text-white"> signup</Link>}
+       { !token && <Link to="/signin" className="text-white">Signin</Link>}
+      { token &&  <button onClick={logout} className="text-white font-serif text-lg">signOut</button>}
+       { !token && <Link to='/signup' className="text-white"> signup</Link>}
 
         </div>
       </div>
